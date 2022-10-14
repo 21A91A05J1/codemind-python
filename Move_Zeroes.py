82cd2161a351946976e694a646e2a178af324d3f
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-10-14 19:32:13 +0530

    n=int(input()) a=list(map(int,input().strip().split()))[:n] c=0 for i in a:     if(i!=0):         print(i,end=' ')     else:         c+=1 for i in range(0,c):     print(0,end=' ')

---
+++ Move_Zeroes.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().strip().split()))[:n]
+c=0
+for i in a:
+    if(i!=0):
+        print(i,end=' ')
+    else:
+        c+=1
+for i in range(0,c):
+    print(0,end=' ')
+    
